d4dd05cf8d9fcde921a03ffaa9bf6736fd024496
kohasuke <110949581+kobayashi1013@users.noreply.github.com>
2024-06-04 00:56:58 +0900

    push

--- dev/dev423.py
+++ dev/dev423.py
@@ -3,6 +3,14 @@ sys.path.append(os.pardir)
 import numpy as np
 from dataset.mnist import load_mnist
 
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        y = y.reshape(1, y.size)
+        t = t.reshape(1, t.size)
+    
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size
+
 (x_train, t_train), (x_test, t_test) = \
     load_mnist(normalize = True, one_hot_label = True)
 
