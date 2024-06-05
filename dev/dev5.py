d8ded0b664731cc7c120b82ca2c1e229acb664ae
kohasuke <110949581+kobayashi1013@users.noreply.github.com>
2024-06-06 02:18:20 +0900

    push

---
+++ dev/dev5.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+
+        return out
+    
+    def backward(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+
+        return dx, dy
+
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+    
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+
+class Relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+    
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+
+        return dx
+
+x = np.array([[1.0, -0.5], [-2.0, 3.0]])
+relu_layer = Relu()
+ret = relu_layer.forward(x)
+print(ret)
+
+dret = np.array([[1.0, 1.0], [1.0, 1.0]])
+dx = relu_layer.backward(dret)
+print(dx)
