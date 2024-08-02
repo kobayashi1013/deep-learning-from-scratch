094eb6e7ed629c72e57c4e2bcdbc294ef9031a55
kohasuke <110949581+kobayashi1013@users.noreply.github.com>
2024-08-02 11:59:37 +0900

    push

--- dev/dev6.py
+++ dev/dev6.py
@@ -112,9 +112,9 @@ class SoftmaxWithLoss:
 class TwoLayerNet:
     def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
         self.params = {}
-        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['W1'] = np.sqrt(2 / input_size) * np.random.randn(input_size, hidden_size)
         self.params['b1'] = np.zeros(hidden_size)
-        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['W2'] = np.sqrt(2 / hidden_size) * np.random.randn(hidden_size, output_size)
         self.params['b2'] = np.zeros(output_size)
 
         self.layers = OrderedDict()
